9bd44e841cdef40d9306f2eddb173139b93a2d32
TyoRan <38074706+TyoRan@users.noreply.github.com>
2019-12-18 07:45:42 +0900

    Update bitmapFileHeader.h

--- 2048_button/bitmapFileHeader.h
+++ 2048_button/bitmapFileHeader.h
@@ -1,20 +1,49 @@
-#ifndef _LIB_BITMAP_H_
-#define _LIB_BITMAP_H_
+#ifndef _BITMAP_FILE_HEADER_
+#define _BITMAP_FILE_HEADER_
 
-#include <stdint.h>
+//Bitmap Header.
+// File Header
+typedef struct 
+{
+    unsigned int    bfSize;         // 4 byte
+    unsigned short  bfReserved1;    // 2 byte
+    unsigned short  bfReserved2;    // 2 byte
+    unsigned int    bfOffBits;      // 4 byte
+} BITMAPFILEHEADER;
 
-//#define ENABLED_DOUBLE_BUFFERING	1
-#define SIZE 4
+// Image Header
+typedef struct 
+{
+    unsigned int    biSize;             // 4 byte
+    unsigned int    biWidth;            // 4 byte
+    unsigned int    biHeight;           // 4 byte
+    unsigned short  biPlanes;           // 2 byte
+    unsigned short  biBitCount;         // 2 byte
+    unsigned int    biCompression;      // 4 byte
+    unsigned int    biSizeImage;        // 4 byte
+    unsigned int    biXPelsPerMeter;    // 4 byte
+    unsigned int    biYPelsPerMeter;    // 4 byte
+    unsigned int    biClrUsed;          // 4 byte
+    unsigned int    biClrImportant;     // 4 byte
+} BITMAPINFOHEADER;
 
-int read_bmp();
-int close_bmp(void);
+// Color Table
+typedef struct 
+{
+    unsigned char   rgbBlue;        // 1 byte
+    unsigned char   rgbGreen;       // 1 byte
+    unsigned char   rgbRed;         // 1 byte
+    unsigned char   rgbReserved;    // 1 byte
+} RGBQUAD;
 
-int fb_init(int * screen_width, int * screen_height, int * bits_per_pixel, int * line_length);
-void fb_close(void);
-void fb_clear(void);
-void fb_write(uint8_t board[SIZE][SIZE]);
-void fb_write_reverse(char *picData, int picWidth, int picHeight);
-void fb_doubleBufSwap(void);
-void fillbox(int i, int j, uint8_t num);
+// Pixel Data
+typedef struct 
+{
+    BITMAPINFOHEADER    bmiHeader;
+    RGBQUAD             bmiColors[1];
+} BITMAPINFO;
+
+#define BIT_VALUE_24BIT   24
+#define FBDEV_FILE  "/dev/fb0"
 
 #endif
